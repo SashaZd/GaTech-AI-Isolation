12a05be3ffae710e87679a51c120b0ca868eda53
SashaZd <sasha@lawn-143-215-113-48.lawn.gatech.edu>
2014-12-03 05:13:52 -0500

    New Check Legal

--- basicLogic.js
+++ basicLogic.js
@@ -1,4 +1,15 @@
+// (in board) -1 :: Empty Space 
+// (-1) : Initial empty arrays 
+
 //////GLOBAL VARIABLES //////
+
+var board = [];
+for(var i=0; i<8; i++){
+	board[i]=[];
+	for(var j=0; j<8; j++){
+		board[i][j] = -1;
+	}
+}
 
 var ERR1 = 0;		//If the square has been filled before and the user keeps clicking it. 
 var TURN = 0;
@@ -59,155 +70,73 @@ function whoseTurn () {
 }
 
 
-function checkIfLegal (index, turnVal) {
-	//console.log("Inside Check Legal");
+function checkIfLegal(index, turnVal){
+
 	// debugger;
-	var legal = false;
+
+	var x1, y1;
+	[x2, y2] = convertToRowCol(index);
+	
 	switch(turnVal){
-		case "X":
-			//console.log(x1y1, x2y2);
-			x1y1  = convertToRowCol(EARLIER_POS[0]);
-			x2y2 = convertToRowCol(index);
-
-			//console.log(x1y1, x2y2);
+		case "X" : 
+			[x1,y1] = convertToRowCol(EARLIER_POS[0]);
 			break;
-		case "O":
-			x1y1 = convertToRowCol(EARLIER_POS[1]);
-			x2y2 = convertToRowCol(index);
+		case "O" : 
+			[x1,y1] = convertToRowCol(EARLIER_POS[1]);
 			break;
-		case "V":
-			x1y1 = convertToRowCol(EARLIER_POS[2]);
-			x2y2 = convertToRowCol(index);
+		case "V" : 
+			[x1,y1] = convertToRowCol(EARLIER_POS[2]);
 			break;
 	}
-
-	if (x1y1[0]==-1){
-		 return true;
+		
+	//If starting position
+	if (x1 == -1){
+		return true;
 	}
-	else { 
-		var x1 = x1y1[0], y1=x1y1[1], x2=x2y2[0], y2=x2y2[1];
 
-		for(var i=1; i<=8; i++){
-
-			if((x1+i) < 8) {									//check that the increasing diagonal doesn't leave the board
-				if(checkIfEmpty(convertToIndex((x1+i), y1), turnVal)){		//check if all the cells between the two moves are empty
-					if((x1+i)==x2 && y1==y2){
-						return true;
-					}
-				}
-				else 
-					return false;
+	//If same row
+	if(x1 == x2){
+		// var startX = 
+		var temp = Math.abs(y1-y2);
+		for (var i = 1, yi = Math.min(y1,y2); i<temp; i++, yi++){
+			console.log("Check Row: ",x1,yi,checkIfEmpty(convertToIndex(x1,yi)))
+			if (!checkIfEmpty(convertToIndex(x1,yi))) {
+				return false;
 			}
-			if((x1-i) >= 0) {									//check that the increasing diagonal doesn't leave the board
-				if(checkIfEmpty(convertToIndex((x1-i), y1), turnVal)){		//check if all the cells between the two moves are empty
-					if((x1-i)==x2 && y1==y2){
-						return true;
-					}
-				}
-				else 
-					return false;
-			}
-
-			if((y1+i) < 8) {									//check that the increasing diagonal doesn't leave the board
-				if(checkIfEmpty(convertToIndex(x1, (y1+i)), turnVal)){		//check if all the cells between the two moves are empty
-					if((x1+i)==x2 && y1==y2){
-						return true;
-					}
-				}
-				else 
-					return false;
-			}
-			if((y1-i) >= 0) {									//check that the increasing diagonal doesn't leave the board
-				if(checkIfEmpty(convertToIndex(x1, (y1-i)), turnVal)){		//check if all the cells between the two moves are empty
-					if((x1-i)==x2 && y1==y2){
-						return true;
-					}
-				}
-				else 
-					return false;
-			}
-			
-			// if(x2y2[0]==x1y1[0] || x2y2[1]==x1y1[1]){
-			// 	return true;		//without checking for barricades
-			// }
-			// else {
-				
-				// console.log(x1, y1, " --------> ", x2, y2);
-					if( ((x1+i) < 8) && ((y1+i) < 8)){									//check that the increasing diagonal doesn't leave the board
-						if(checkIfEmpty(convertToIndex((x1+i), (y1+i)), turnVal)){		//check if all the cells between the two moves are empty
-							if(((x1+i)==x2) && ((y1+i)==y2)){							//check if the new cell is on the diagonal
-								return true;
-							}	
-						}
-						else{											//there was a barricade in the middle
-							// legal = false;
-							return false;
-						}
-					}
-
-					if( (x1-i) >= 0 && (y1+i) < 8 ) {			//check the upper-right bounds
-						if(checkIfEmpty(convertToIndex((x1-i), (y1+i)), turnVal)){		//check if all the cells between the two moves are empty
-							if(((x1-i)==x2) && ((y1+i)==y2)){							//check if the new cell is on the diagonal
-								return true;	
-							}
-						}
-						else{											//there was a barricade in the middle
-							// legal = false;
-							return false;
-						}
-					}
-					// debugger;
-					if( ((x1+i) < 8) && ((y1-i) >= 0)){			//check the lower-left bounds
-						if(checkIfEmpty(convertToIndex((x1+i), (y1-i)), turnVal)){		//check if all the cells between the two moves are empty
-							if(((x1+i)==x2) && ((y1-i)==y2)) {							//check if the new cell is on the diagonal
-								return true;
-							}
-						}
-						else{
-																		//there was a barricade in the middle
-							// legal = false;
-							return false;
-						}
-					}
-					if( (x1-i) >= 0 && (y1-i) >= 0 ){			//check the upper-left bounds
-						if(checkIfEmpty(convertToIndex((x1-i), (y1-i)), turnVal)){		//check if all the cells between the two moves are empty
-							if(((x1-i)==x2) && ((y1-i)==y2)){							//check if the new cell is on the diagonal
-								return true;
-							}
-						}
-						else{											//there was a barricade in the middle
-							// legal = false;
-							return false;
-						}
-					}
-				
-
-			// }
 		}
+		return true;
 	}
-	return legal;
+
+	//If same Column
+	if(y1 == y2){
+		var temp = Math.abs(x1-x2);
+		for (var i = 1, xi = Math.min(x1,x2); i<temp; i++, xi++){
+			console.log("Check Column: ",xi,y1,checkIfEmpty(convertToIndex(xi,y1)))
+			if (!checkIfEmpty(convertToIndex(xi,y1))) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	//If diagonal
+	if(Math.abs(x2-x1) == Math.abs(y2-y1)){
+		return false;
+	}
+
+	return false;
 }
 
 
-function checkIfEmpty(index, turnVal) {
+function checkIfEmpty(index) {
 	
-	$("#chess div").text(function(index2){
-		if(index2 == index){
-			el=this;
-		}
-	});
-	// console.log(el);
-	var contains = el.textContent;
+	[x,y] = convertToRowCol(index);
 
-	// console.log("Is there a move in here", contains.length);
-	if (contains=="") {			//the square is empty, can be filled 
-		console.log("Empty cell, ", convertToRowCol(index));
+	if(board[x][y]==-1){
 		return true;
-		
 	}
 	else {
-		console.log("Filled cell, ", convertToRowCol(index));
-		return false;			//the square already has a player's move in it
+		return false;
 	}
 }
 
@@ -235,44 +164,44 @@ function notLegal (error) {
 			break;
 	}
 }
+var tempPos;
 
 $(document).ready(function() {
 	$("#chess div").click(function() {
 		var index = $("#chess div").index(this);
-		//console.log(index, " being tried");
+
 		var turnVal = whoseTurn();					//check whose turn it is
 
-		if(checkIfEmpty(index, turnVal)){
-
-			var tempPos = EARLIER_POS; console.log("Earlier : ", EARLIER_POS)
-			EARLIER_POS = CURRENT_POS; console.log("New : ", CURRENT_POS)
-			
+		if(checkIfEmpty(index)){
+			// tempPos = EARLIER_POS;
+				
 			if(checkIfLegal(index, turnVal)){
-			
+				
+				
 				//marks the cell with X/O/V and crosses out old cells
 				$("#chess div").text(function(index2){			//iterating through all the squares
 					if (index2 == index){						//if the sq == the sq that was clicked on, then increment the turn, and print X/O/V
+
 						TURN += 1;
+						[x,y] = convertToRowCol(index);
 
 						switch(turnVal){
 							case "X":
-								// //console.log(EARLIER_POS[0], CURRENT_POS[0]);
-								EARLIER_POS[0] = CURRENT_POS[0];
-								CURRENT_POS[0] = index;
-								// //console.log(EARLIER_POS[0], CURRENT_POS[0]);
+								EARLIER_POS[0] = index
+								board[x][y]=0;
 								break;
 							case "O":
-								EARLIER_POS[1] = CURRENT_POS[1];
-								CURRENT_POS[1]=index;
+								EARLIER_POS[1] = index;
+								board[x][y]=1;
 								break;
 							case "V":
-								EARLIER_POS[2] = CURRENT_POS[2];
-								CURRENT_POS[2]=index;
+								EARLIER_POS[2] = index;
+								board[x][y]=2;
 								break;
 						}
 
 
-						return turnVal;
+						return turnVal;			//actual marking of div done here
 					}
 					else {
 						//change the older X/O/V that exists to be displayed as a crossed out square
@@ -281,8 +210,6 @@ $(document).ready(function() {
 				});
 			}
 			else { 
-				// CURRENT_POS = EARLIER_POS;
-				EARLIER_POS = tempPos;
 				var error = "Not Legal";
 				notLegal(error);
 				// pass;
